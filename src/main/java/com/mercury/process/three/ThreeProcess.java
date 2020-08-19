3bc3c59fba2e6efda9058822064b9b079e52ccb6
나규태 <kyoutae@ybtour.co.kr>
2020-08-19 15:30:13 +0900

    error update

--- src/main/java/com/mercury/process/three/ThreeProcess.java
+++ src/main/java/com/mercury/process/three/ThreeProcess.java
@@ -2,40 +2,24 @@ package com.mercury.process.three;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
-import com.mercury.jpa.model.love.Love;
 import com.mercury.jpa.model.three.Three;
-import com.mercury.jpa.model.user.User;
 import com.mercury.jpa.model.word.Word;
-import com.mercury.jpa.repository.love.LoveRepository;
 import com.mercury.jpa.repository.three.ThreeRepository;
-import com.mercury.jpa.repository.user.UserRepository;
 import com.mercury.jpa.repository.word.WordRepository;
-import com.mercury.util.DateUtil;
 
 @Component
 @SuppressWarnings("unchecked")
 public class ThreeProcess {
-	private static final Logger LOGGER = LogManager.getLogger(ThreeProcess.class);
-	
 	@Autowired
 	private ThreeRepository threeRepository;
 
 	@Autowired
 	private WordRepository weekWordRepository;
-	
-	@Autowired
-	private LoveRepository loveRepository;
-	
-	@Autowired
-	private UserRepository userRepository;
 	
 	public <T extends Object> T getTotalPoint(String userIdx) throws Exception {
 		try {
@@ -91,50 +75,5 @@ public class ThreeProcess {
 		}
 		
 		return (T) three;
-	}
-
-	public <T extends Object> T inThree(Three three) throws Exception {
-		return (T) threeRepository.save(three);
-	}
-	public <T extends Object> T upThree(Three three) throws Exception {
-		
-		Three dbThree = threeRepository.findByIdx(three.getIdx());
-		
-		if(!dbThree.getPoint().equals(three.getPoint())) {
-			
-			Love love = new Love();
-			love.setIdx(UUID.randomUUID().toString().replace("-", ""));
-			love.setInsertDate(DateUtil.now());
-			love.setContentIdx(three.getIdx());
-			
-			User user = userRepository.findByUsername(three.getLoveName());
-			
-			love.setUserIdx(user.getIdx());
-			
-			loveRepository.save(love);
-			three.setLoveName("");
-		}
-		
-		return (T) threeRepository.save(three);
-	}
-	
-	public <T extends Object> T deThree(Three three) throws Exception {
-		try {
-			threeRepository.delete(three);
-			return (T) Boolean.TRUE;
-		} catch (Exception e) {
-			 e.printStackTrace();
-			 return (T) e;
-		}
-	}
-	
-	public <T extends Object> T deThreeAllEntities(List<Three> threes) throws Exception {
-		try {
-			threeRepository.deleteAll(threes);
-			return (T) Boolean.TRUE;
-		} catch (Exception e) {
-			 e.printStackTrace();
-			 return (T) e;
-		}
 	}
 }
--- src/main/java/com/mercury/process/two/TwoProcess.java
+++ src/main/java/com/mercury/process/two/TwoProcess.java
@@ -1,21 +1,15 @@
 package com.mercury.process.two;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
-import com.mercury.jpa.model.love.Love;
 import com.mercury.jpa.model.two.Two;
-import com.mercury.jpa.model.user.User;
 import com.mercury.jpa.model.word.Word;
-import com.mercury.jpa.repository.love.LoveRepository;
 import com.mercury.jpa.repository.two.TwoRepository;
-import com.mercury.jpa.repository.user.UserRepository;
 import com.mercury.jpa.repository.word.WordRepository;
-import com.mercury.util.DateUtil;
 
 @Component
 @SuppressWarnings("unchecked")
@@ -26,12 +20,6 @@ public class TwoProcess {
 
 	@Autowired
 	private WordRepository weekWordRepository;
-	
-	@Autowired
-	private LoveRepository loveRepository;
-	
-	@Autowired
-	private UserRepository userRepository;
 	
 	public <T extends Object> T getTotalPoint(String userIdx) throws Exception {
 		try {
@@ -65,51 +53,5 @@ public class TwoProcess {
 		List<Two> two = twoRepository.findByWordIdx(dto.getIdx(), Sort.by(Sort.Direction.DESC, "insertDate"));
 		
 		return (T) two;
-	}
-
-	public <T extends Object> T inTwo(Two two) throws Exception {
-		return (T) twoRepository.save(two);
-	}
-	
-	public <T extends Object> T upTwo(Two two) throws Exception {
-		
-		Two dbTwo = twoRepository.findByIdx(two.getIdx());
-		
-		if(!dbTwo.getPoint().equals(two.getPoint())) {
-			
-			Love love = new Love();
-			love.setIdx(UUID.randomUUID().toString().replace("-", ""));
-			love.setInsertDate(DateUtil.now());
-			love.setContentIdx(two.getIdx());
-			
-			User user = userRepository.findByUsername(two.getLoveName());
-			
-			love.setUserIdx(user.getIdx());
-			
-			loveRepository.save(love);
-			two.setLoveName("");
-		}
-		
-		return (T) twoRepository.save(two);
-	}
-	
-	public <T extends Object> T deTwo(Two two) throws Exception {
-		try {
-			twoRepository.delete(two);
-			return (T) Boolean.TRUE;
-		} catch (Exception e) {
-			 e.printStackTrace();
-			 return (T) e;
-		}
-	}
-	
-	public <T extends Object> T deTwoAllEntities(List<Two> twos) throws Exception {
-		try {
-			twoRepository.deleteAll(twos);
-			return (T) Boolean.TRUE;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return (T) e;
-		}
 	}
 }

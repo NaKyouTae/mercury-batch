bba7e6e851f6125c009597503af1cd03762f36f2
나규태 <kyoutae@ybtour.co.kr>
2021-04-08 17:27:54 +0900

    origin Allowed update

--- src/main/java/com/mercury/MercuryBatchApplication.java
+++ src/main/java/com/mercury/MercuryBatchApplication.java
@@ -32,7 +32,7 @@ public class MercuryBatchApplication implements WebMvcConfigurer {
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4300", "http://127.0.0.1:4300")
+		registry.addMapping("/**").allowedOrigins("*")
 				.allowedMethods("GET", "POST", "PUT", "DELETE").exposedHeaders("AWT", "RWT", "UWT")
 				.allowCredentials(true).maxAge(3600);
 	}
